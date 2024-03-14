83511586be916b25b1afa19d39cca85f137a446f
sindhia12 <Sindhiagaad@gmail.com>
2024-03-14 23:12:03 +0500

    first commit

---
+++ nested.ts
@@ -0,0 +1,29 @@
+let information={
+    name:{
+        firstname:"Sindhia",
+        Surname:"siddiqui",
+        lastname:"ayaz",
+
+    },
+    age:24,
+    single:true,
+    familymembers:5,
+    familymembersname:["affan","rumaisa","areeba","shahida","ayaz"],
+   education: {
+              Qualification: "Masters",
+              majorsubjects: ["economics","Human resource","development economics"],
+              completed:true,
+
+    },
+     contact: {
+        mobileno:2468,
+        email:"sindhia01@gmail.com",
+      },
+    };
+
+    console.log(information.contact.mobileno);
+    console.log(information.education.majorsubjects[2]);
+    console.log(information.name.firstname);
+    console.log(information.age);
+    console.log(information.familymembers);
+    
---
+++ object.js
@@ -0,0 +1,13 @@
+/// objects
+var student = {
+    fullName: "ayesha",
+    lastName: "tariq",
+    class: 1,
+    age: 12,
+    ispassed: true,
+    Subjects: ["english", "urdu", "islamiat",],
+};
+console.log(student.fullName);
+console.log(student.age);
+console.log(student.class);
+console.log(student.ispassed);
